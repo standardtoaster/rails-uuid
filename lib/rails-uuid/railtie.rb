3c783d61b2bb83f7d275d84c4712715a0abd0499
Andrew Preece <andrew.preece@gmail.com>
2011-01-01 18:05:53 -0700

    fixed typo in railtie

--- lib/rails-uuid/railtie.rb
+++ lib/rails-uuid/railtie.rb
@@ -8,7 +8,7 @@ module RailsUUID
     initializer 'activerecord.uuid.pk.support' do |app|  
       
       #only make the changes if the DB type is supported, otherwise log an error. 
-      if UUID_DB_PK_TYPE.contains_key? Rails.configuration.database_configuration[Rails.env]['adapter']
+      if UUID_DB_PK_TYPE.has_key? Rails.configuration.database_configuration[Rails.env]['adapter']
         ActiveRecord::Base.send :include, RailsUUID::ActiveRecordUUID
         ActiveRecord::ConnectionAdapters::TableDefinition.send :include, RailsUUID::TableDefinitionUUID
         ActiveRecord::Base.connection.class.send :include, RailsUUID::AdapterUUID
